589f93f5c4de27b1a7f0836bec0e451206c675e8
b0rn2rage <slashjk3z0r>
2020-07-14 12:41:56 +0300

    Упражнение 3.6.3, вторая финальная версия. Раздел с фикстурами

--- 363.py
+++ 363.py
@@ -2,19 +2,21 @@ import math
 import time
 import pytest
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 @pytest.fixture(scope='function')
 def browser():
-    print('Запускаем браузер')
+    print('\nЗапуск браузера')
     browser = webdriver.Chrome()
     yield browser
-    print('Закрываем браузер')
+    print('\nЗакрытие браузера')
     browser.quit()
 
 
-class TestCorrectAnswer():
+class TestCorrectAnswer:
 
     @pytest.mark.parametrize('link', ['https://stepik.org/lesson/236895/step/1',
                                       'https://stepik.org/lesson/236896/step/1',
@@ -25,16 +27,15 @@ class TestCorrectAnswer():
                                       'https://stepik.org/lesson/236904/step/1',
                                       'https://stepik.org/lesson/236905/step/1'])
     def test_params(self, browser, link):
-        current_link = f'{link}'
-        browser.get(current_link)
-        print(f'Текущий url страницы - {current_link}')
-        time.sleep(5)
-        try:
-            textarea = browser.find_element_by_css_selector('.textarea.string-quiz__textarea')
-        except NoSuchElementException:
-            print('textarea не найден!')
-        answer = math.log(int(time.time() - 77.3))
+        browser.get(link)
+        print(f'\nТекущий url страницы - {link}')
+        textarea = WebDriverWait(browser, 10).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, '.textarea.string-quiz__textarea')))
+        answer = math.log(int(time.time() - 67))
         textarea.send_keys(str(answer))
         submit_button = browser.find_element_by_class_name('submit-submission')
         submit_button.click()
-        time.sleep(5)
+        check_message = WebDriverWait(browser, 10).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, '.smart-hints__hint')))
+        check_message = check_message.text
+        assert check_message == 'Correct!', f'Отображается некорректное сообщение {check_message}'
